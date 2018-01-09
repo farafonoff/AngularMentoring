d5c54989afbd2d24d0e7810dc1d41001145a3bc1
Artem Farafonov <artem_farafonov@epam.com>
2018-01-09 16:18:27 +0400

    part 1 done

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,27 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../model/user.model';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isLoggedIn: boolean;
+  user: User;
+  subscriptions = [];
 
-  get isLoggedIn(): boolean {
-    return this.authService.isAuthenticated();
+  constructor(private authService: AuthService) {
   }
 
-  get user(): User {
-    return this.authService.getUserInfo();
-  }
-
-  constructor(private authService: AuthService) { }
-  
   ngOnInit() {
+    this.subscriptions.push(this.authService.getUserInfo().subscribe(user => {
+      this.user = user;
+    }));
+    this.subscriptions.push(this.authService.isAuthenticated().subscribe(isAuth => {
+      this.isLoggedIn = isAuth;
+    }));
   }
-  
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s());
+  }
+
   logout() {
     this.authService.logout();
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
   private AUTH_STORAGE_KEY = 'auth';
-  constructor() { }
+  authUser = new ReplaySubject<User>(1);
+  constructor() {
+    this.authUser.subscribe(user => {// save to LS
+      if (user != null) {
+        localStorage.setItem(this.AUTH_STORAGE_KEY, JSON.stringify(user));
+      } else {
+        localStorage.removeItem(this.AUTH_STORAGE_KEY);
+      }
+    });
+    if (!!localStorage.getItem(this.AUTH_STORAGE_KEY)) {
+      this.authUser.next(
+        JSON.parse(localStorage.getItem(this.AUTH_STORAGE_KEY))
+      );
+    }
+   }
 
   login(user: string, password: string) {
-    localStorage.setItem(this.AUTH_STORAGE_KEY, JSON.stringify(new User(user, password)));
+    const newUser = new User(user, password);
+    this.authUser.next(newUser);
   }
 
   logout() {
-    localStorage.removeItem(this.AUTH_STORAGE_KEY);
-  }
-  
-  isAuthenticated():boolean {
-    return !!localStorage.getItem(this.AUTH_STORAGE_KEY);
+    this.authUser.next(null);
   }
 
-  getUserInfo():User {
-    return JSON.parse(localStorage.getItem(this.AUTH_STORAGE_KEY));
+  isAuthenticated(): Observable<boolean> {
+    return this.authUser.map(u => !!u);
+  }
+
+  getUserInfo(): Observable<User> {
+    return this.authUser;
   }
 }
