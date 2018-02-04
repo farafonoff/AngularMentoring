68b1653183a859bc346e323819502050b25b339c
farafonoff <farafonoff@gmail.com>
2018-02-05 00:01:01 +0400

    date component work

--- src/app/pages/course-add/control-date/control-date.component.ts
+++ src/app/pages/course-add/control-date/control-date.component.ts
@@ -9,6 +9,7 @@ import {
   ValidationErrors
 } from '@angular/forms';
 import { noop } from 'lodash';
+import * as moment from 'moment';
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -17,13 +18,7 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 }
 export const CUSTOM_INPUT_CONTROL_VALIDATORS: any = {
   provide: NG_VALIDATORS,
-  useValue: (c: FormControl) => {
-    if (!c.value.match('[0-9][0-9]/[0-9][0-9]/[0-9][0-9]([0-9][0-9])?')) {
-      
-    } else {
-      return null;
-    }
-  },
+  useExisting: forwardRef(() => ControlDateComponent),
   multi: true
 }
 @Component({
@@ -32,14 +27,14 @@ export const CUSTOM_INPUT_CONTROL_VALIDATORS: any = {
   styleUrls: ['./control-date.component.css'],
   providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR, CUSTOM_INPUT_CONTROL_VALIDATORS]
 })
-export class ControlDateComponent implements OnInit, ControlValueAccessor {
+export class ControlDateComponent implements OnInit, ControlValueAccessor, Validator {
   private innerValue: string = '';
   private onTouchedCallback: () => void = noop;
   private onChangeCallback: (_: any) => void = noop;
   
   writeValue(obj: any): void {
-    console.log('writeValue ', obj)
-    this.innerValue = obj;
+    console.log('writeValue ', obj);
+    this.innerValue = moment(obj).format('DD/MM/YYYY');
   }
   registerOnChange(fn: any): void {
     this.onChangeCallback = fn;
@@ -61,14 +56,21 @@ export class ControlDateComponent implements OnInit, ControlValueAccessor {
   }
 
   onChange(evt: Event) {
-    this.onChangeCallback(this.innerValue);
+    this.onChangeCallback(moment(this.innerValue, 'DD/MM/YYYY').toDate());
   }
 
   validate(c: AbstractControl): ValidationErrors {
-    return {};
+    let valid = !!this.innerValue.match('[0-9][0-9]/[0-9][0-9]/[0-9][0-9]([0-9][0-9])?');
+    if (valid) {
+      return null;
+    } else {
+      return {
+        validDate: false
+      };
+    }
   }
 
-  //get accessor
+  /*//get accessor
   get value(): any {
     return this.innerValue;
   };
@@ -79,6 +81,6 @@ export class ControlDateComponent implements OnInit, ControlValueAccessor {
       this.innerValue = v;
       this.onChangeCallback(v);
     }
-  }
+  }*/
 
 }
