58ec83590026e8831dc38ea2a2a2e53e49a46678
Artem Farafonov <artem_farafonov@epam.com>
2018-02-08 15:53:42 +0400

    404 error in subroute

--- src/app/services/courses-resolver.service.ts
+++ src/app/services/courses-resolver.service.ts
@@ -3,15 +3,22 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 import { CoursesService } from './courses.service';
 import { Course } from '../model/course.model';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class CoursesResolverService implements Resolve<Course> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
     const id = route.params.id;
     console.log(id);
-    return this.coursesService.findById(id);
+    return this.coursesService
+      .findById(id)
+      .catch(error => {
+        this.router.navigate(['/missing-course']);
+        return Observable.of(null);
+      });
   }
 
-  constructor(private coursesService: CoursesService) {  }
+  constructor(private coursesService: CoursesService, private router: Router) {  }
 
 }
